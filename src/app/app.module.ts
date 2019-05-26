931803a6c62381df3e7a06df4a8357d92891f2db
Jose Alejandro Rojano Crisson <j053noir@gmail.com>
2019-05-26 17:14:34 -0500

    Created material module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MaterialModule } from './material.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +15,7 @@ import { NavigationComponent } from './navigation/navigation.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import {
+  MatFormFieldModule,
+  MatInputModule,
+} from '@angular/material';
+
+@NgModule({
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+  ],
+
+  exports: [
+    MatFormFieldModule,
+    MatInputModule,
+  ],
+})
+export class MaterialModule {}
